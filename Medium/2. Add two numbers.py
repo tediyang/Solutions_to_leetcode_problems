9b116393b7d848e231ed860adca3122ede781c28
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-22 12:14:53 +0100

    2-add numbers

---
+++ Medium/2. Add two numbers.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        res = dummy = ListNode()
+        remain = 0
+        
+        while l1 or l2:
+            v1, v2 = 0, 0
+            
+            if l1:
+                v1, l1 = l1.val, l1.next
+            if l2:
+                v2, l2 = l2.val, l2.next
+            
+            sum_ = v1 + v2 + remain
+            res.next = ListNode(sum_ % 10)
+            res = res.next
+            remain = sum_ // 10
+        
+        if remain:
+            res.next = ListNode(remain)
+        
+        return dummy.next
