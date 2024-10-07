febc39fb0a9bc5ecdba07bd0abbbe12cbad9c40d
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2024-10-07 08:59:32 +0100

    update the code to have speed complexity of O(n + m) instead of O(n + m)^2

--- Python/Medium/1813. Sentence Similarity III.py
+++ Python/Medium/1813. Sentence Similarity III.py
@@ -4,25 +4,21 @@ class Solution:
     sentence2 = sentence2.split(' ')
 
     if len(sentence1) > len(sentence2):
-      pick = sentence2
-      check = sentence1
-    else:
-      pick = sentence1
-      check = sentence2
+      sentence1, sentence2 = sentence2, sentence1
 
-    while len(pick) > 0:
-      if pick[0] == check[0]:
-        pick.pop(0)
-        check.pop(0)
+    pick_left, pick_right = 0, len(sentence1) - 1
+    check_left, check_right = 0, len(sentence2) - 1
+
+    while pick_left <= pick_right:
+      if sentence1[pick_left] == sentence2[check_left]:
+        pick_left += 1
+        check_left += 1
   
-      elif pick[-1] == check[-1]:
-        pick.pop()
-        check.pop()
+      elif sentence1[pick_right] == sentence2[check_right]:
+        pick_right -= 1
+        check_right -= 1
 
       else:
         break
-
-    if len(pick) == 0:
-      return True
     
-    return False
+    return pick_left > pick_right
